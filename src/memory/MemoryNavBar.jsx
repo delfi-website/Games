b04b55edc856b125c27d1da03e05f3a9acca7532
Delfi-CH <laspitzer@gmx.ch>
2025-03-28 13:14:17 +0100

    made package.json for deploying on gh-pages

--- src/memory/MemoryNavBar.jsx
+++ src/memory/MemoryNavBar.jsx
@@ -6,20 +6,21 @@ export default function MemoryNavBar({ callback, callback2, callback3 }) {
     const [pointsB, setPointsB] = useState(0);
 
     function reset() {
-        callback3
-        setPointsA(0)
-        setPointsB(0)
+        callback2();
+        setPointsA(0);
+        setPointsB(0);
+        callback3();
     }
 
-    if (pointsA === 7) {
-        alert("Player A has won!")
-        reset()
-    } else if(pointsB===7){
-        alert("Player B has won!")
-        reset()
-    } else if(pointsA+pointsB===12) {
-        alert("Its a tie!")
-        reset()
+    if (pointsA === 4) {
+        alert("Player A has won!");
+        reset();
+    } else if (pointsB === 4) {
+        alert("Player B has won!");
+        reset();
+    } else if (pointsA + pointsB === 6) {
+        alert("It's a tie!");
+        reset();
     }
 
     return (
@@ -27,7 +28,7 @@ export default function MemoryNavBar({ callback, callback2, callback3 }) {
             <h2>Memory</h2>
             <h3>Player A has {pointsA} points. | Player B has {pointsB} points.</h3>
             <input type="button" value="Add Points to A" onClick={() => setPointsA(pointsA + 1)} />
-            <input type="button" value="Reset" onClick={reset   } />
+            <input type="button" value="Reset" onClick={reset} />
             <input type="button" value="Add Points to B" onClick={() => setPointsB(pointsB + 1)} />
             <h1 className="filler">Filler</h1>
         </div>
